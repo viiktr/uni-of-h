bcbc72ddcb0d84d5341226338c577378288fdcd7
Vik <85831468+viiktr@users.noreply.github.com>
2023-02-13 00:22:08 +0200

    Create Single page app diagram.js

---
+++ full-stack/Single page app diagram.js
@@ -0,0 +1,12 @@
+---
+Single-page app version of the notes app
+---
+sequenceDiagram
+    participant User
+    participant Browser
+    participant Server
+
+    User->>Browser: Navigates to https://studies.cs.helsinki.fi/exampleapp/spa
+    Browser->>Server: Sends a GET request for the single page app page
+    Server->>Browser: Returns the HTML, CSS, and JavaScript for the single page app
+    Browser->>User: Renders the single page app, including the note list and form for creating new notes
